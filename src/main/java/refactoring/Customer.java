cf354c87f26e49bc22f3224ca8583a923c6af962
V. Julião <viniciusjuliao@dcc.ufmg.br>
2020-10-21 09:25:06 -0300

    Commit 4

--- src/main/java/refactoring/Customer.java
+++ src/main/java/refactoring/Customer.java
@@ -26,7 +26,7 @@ public class Customer {
         String result = "Rental Record for " + getName() + "\n";
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
-            double thisAmount = this.amountFor(each);
+            double thisAmount = each.getCharge();
 
             // add frequent renter points
             frequentRenterPoints ++;
@@ -45,26 +45,5 @@ public class Customer {
         result += "You earned " + String.valueOf(frequentRenterPoints) +
                 " frequent renter points";
         return result;
-    }
-
-    //determine amounts for each line
-    private double amountFor(Rental aRental){
-        double thisAmount = 0.0;
-        switch (aRental.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (aRental.getDaysRented() > 2)
-                    thisAmount += (aRental.getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += aRental.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (aRental.getDaysRented() > 3)
-                    thisAmount += (aRental.getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return thisAmount;
     }
 }
