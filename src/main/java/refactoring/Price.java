34eab621bacab9854ae8b65001ba72ea5edd0479
V. Julião <viniciusjuliao@dcc.ufmg.br>
2020-10-21 10:37:15 -0300

    Commit 15

--- src/main/java/refactoring/Price.java
+++ src/main/java/refactoring/Price.java
@@ -5,12 +5,7 @@ public abstract class Price {
 
     public abstract double getCharge(int daysRented);
 
-    public int getFrequentRenterPoints(int daysRented){
-        // add frequent renter points
-        int frequentRenterPoints = 1;
-        // add bonus for a two day new release rental
-        if ((this.getPriceCode() == Movie.NEW_RELEASE) &&
-                daysRented > 1) frequentRenterPoints ++;
-        return frequentRenterPoints;
+    public int getFrequentRenterPoints(int daysRented) {
+        return 1;
     }
 }
